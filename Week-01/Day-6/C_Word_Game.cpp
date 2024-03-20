5c7647d8267802c2ca5cf634dc742fe5baed82c8
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2024-03-20 23:05:56 +0600

    day-6

--- Week-01/Day-6/C_Word_Game.cpp
+++ Week-01/Day-6/C_Word_Game.cpp
@@ -13,28 +13,62 @@ int main()
         int n;
         cin >> n;
 
+        int score[3] = {0};
+
         string str[3][n];
+        map<string, int> mp[3];
 
-        map<string, int> mp1;
-        map<string, int> mp2;
-        map<string, int> mp3;
-
-        for (int k = 0; k < 3; k++)
+        for (int i = 0; i < 3; i++)
         {
-            for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
             {
-                cin >> str[k][i];
+                string tmp;
+                cin >> tmp;
+                str[i][j] = tmp;
+                mp[i][tmp]++;
             }
         }
 
-        int score[3] = {0};
+        for (int i = 0; i < n; i++)
+        {
+            if (mp[1][str[0][i]] == 0 && mp[2][str[0][i]] == 0)
+            {
+                score[0] += 3;
+            }
+            else if (mp[1][str[0][i]] == 0 || mp[2][str[0][i]] == 0)
+            {
+                score[0]++;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (mp[0][str[1][i]] == 0 && mp[2][str[1][i]] == 0)
+            {
+                score[1] += 3;
+            }
+            else if (mp[0][str[1][i]] == 0 || mp[2][str[1][i]] == 0)
+            {
+                score[1]++;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (mp[0][str[2][i]] == 0 && mp[1][str[2][i]] == 0)
+            {
+                score[2] += 3;
+            }
+            else if (mp[0][str[2][i]] == 0 || mp[1][str[2][i]] == 0)
+            {
+                score[2]++;
+            }
+        }
 
         for (int i = 0; i < 3; i++)
         {
             cout << score[i] << " ";
         }
-
-        cout << "hello";
+        cout << "\n";
     }
 
     return 0;
