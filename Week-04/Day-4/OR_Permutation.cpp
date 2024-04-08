5717781dbf58cbda18741fa1719d6d5ec53a0cc7
Yeasin Arfat <yeasinarfatemon12345@gmail.com>
2024-04-09 01:49:34 +0600

    file add

--- Week-04/Day-4/OR_Permutation.cpp
+++ Week-04/Day-4/OR_Permutation.cpp
@@ -7,5 +7,21 @@ using ll = long long;
 int main()
 {
 
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+
+        for (int i = n; i > 0; i--)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
